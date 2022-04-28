57d7d4bb070086673f9e60b9bb4baae04588704b
Mjchaurasia9 <92732297+Mjchaurasia9@users.noreply.github.com>
2022-04-28 11:14:48 +0530

    Add files via upload

---
+++ arraySum/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("How many Element You want to Add in Array:\t");
+int sum = 0;
+int num = int.Parse(Console.ReadLine());
+int [] arr = new int [num];
+
+for (int i = 0; i < arr.Length; i++)
+{
+Console.Write($"Enter value for Position number {i+1} & index Number {i}:\t");
+int val = int.Parse(Console.ReadLine());
+arr[i] = val;
+}
+
+Console.WriteLine();
+Console.Write("Your Given value of Array is:\t");
+foreach(int item in arr)
+{
+Console.Write(item+" ");
+sum += item;
+}
+Console.WriteLine();
+Console.WriteLine($"Sum of Given Value is:\t{sum}");
